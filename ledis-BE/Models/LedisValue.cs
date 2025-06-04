54b7cd0394da7cca4206f5a375d40c3abf2c024b
Chien Nguyen Quang <quangchieens1@gmail.com>
2025-06-04 17:03:15 +0700

    chore: add Expiration to LedisValue

--- ledis-BE/Models/LedisValue.cs
+++ ledis-BE/Models/LedisValue.cs
@@ -3,6 +3,7 @@ namespace ledis_BE.Models;
 public abstract class LedisValue
 {
     public abstract LedisValueType Type { get; }
+    public long Expiration { get; set; } = -1; // Default to no expiration
 }
 
 public enum LedisValueType
