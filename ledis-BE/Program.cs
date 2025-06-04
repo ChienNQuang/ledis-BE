a048b68bdc2a7548f5823d0cf9f4f621d3e7d5a1
Chien Nguyen Quang <quangchieens1@gmail.com>
2025-06-04 17:21:26 +0700

    chore: register DataStore as a singleton in ASP.NET Core service provider

--- ledis-BE/Program.cs
+++ ledis-BE/Program.cs
@@ -1,9 +1,13 @@
+using ledis_BE.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<DataStore>();
 
 var app = builder.Build();
 
