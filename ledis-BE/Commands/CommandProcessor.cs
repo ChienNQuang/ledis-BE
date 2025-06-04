df8bfbf59c0063683a56226d6bf843389c6b195e
Chien Nguyen Quang <quangchieens1@gmail.com>
2025-06-04 18:21:30 +0700

    feat: get and set commands

--- ledis-BE/Commands/CommandProcessor.cs
+++ ledis-BE/Commands/CommandProcessor.cs
@@ -1,11 +1,70 @@
 using ledis_BE.Models;
+using ledis_BE.Models.DataStructures;
 
 namespace ledis_BE.Commands;
 
 public static class CommandProcessor
 {
+    private static readonly string[] BaseCommands = ["GET", "SET"];
+
     public static string Process(DataStore dataStore, string command, string[] arguments)
     {
+        string uppercaseCommand = command.ToUpper();
+        byte[][] byteArguments = arguments.Select(Convert.FromBase64String).ToArray();
+        if (BaseCommands.Any(c => c.Equals(uppercaseCommand)))
+        {
+            switch (uppercaseCommand)
+            {
+                case "GET":
+                    return Get(dataStore, byteArguments);
+                case "SET":
+                    return Set(dataStore, byteArguments);
+            }
+        }
+
         return string.Empty;
+    }
+
+    private static string Get(DataStore dataStore, byte[][] arguments)
+    {
+        if (arguments.Length != 1)
+        {
+            return "ERROR: wrong number of arguments for 'get' command";
+        }
+
+        byte[] key = arguments[0];
+
+        if (!dataStore.Data.TryGetValue(key, out LedisValue? value))
+        {
+            return "(nil)";
+        }
+
+        if (value is null)
+        {
+            return "(nil)";
+        }
+
+        if (value is not LedisString stringValue)
+        {
+            return "ERROR: WRONGTYPE Operation against a key holding the wrong kind of value";
+        }
+
+        return $"\"{stringValue}\"";
+    }
+
+    private static string Set(DataStore dataStore, byte[][] arguments)
+    {
+        if (arguments.Length != 2)
+        {
+            return "ERROR: wrong number of arguments for 'set' command";
+        }
+
+        byte[] key = arguments[0];
+        byte[] value = arguments[1];
+
+        dataStore.Data.Remove(key);
+        dataStore.Data.Add(key, new LedisString(value));
+
+        return "OK";
     }
 }
--- ledis-BE/Models/DataStructures/LedisString.cs
+++ ledis-BE/Models/DataStructures/LedisString.cs
@@ -2,5 +2,12 @@ namespace ledis_BE.Models.DataStructures;
 
 public class LedisString : LedisValue
 {
+    public byte[] Value { get; set; }
+
+    public LedisString(byte[] value)
+    {
+        Value = value;
+    }
+
     public override LedisValueType Type => LedisValueType.String;
 }
