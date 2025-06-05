ffaa7d444b986530eee138e7de1e7eb4f0d88d25
Chien Nguyen Quang <quangchieens1@gmail.com>
2025-06-05 20:33:29 +0700

    feat: implements LedisSet

--- ledis-BE/Models/Set/LedisSet.cs
+++ ledis-BE/Models/Set/LedisSet.cs
@@ -1,6 +1,36 @@
+using ledis_BE.Models.String;
+
 namespace ledis_BE.Models.Set;
 
 public class LedisSet : LedisValue
 {
+    private readonly ISetValue<IStringValue> _setValue;
+    private readonly LedisSetEncoding _encoding;
+
+    public LedisSet(IEnumerable<string> values)
+    {
+        _setValue = new HashSetSetValue<IStringValue>(ToStringValues(values));
+        _encoding = LedisSetEncoding.Hashtable;
+    }
+
     public override LedisValueType Type => LedisValueType.Set;
+
+    public int SAdd(IEnumerable<string> values)
+    {
+        return _setValue.SAdd(ToStringValues(values));
+    }
+
+    private static IEnumerable<IStringValue> ToStringValues(IEnumerable<string> values)
+    {
+        return values.Select(v =>
+        {
+            (IStringValue value, _) = StringHelpers.GetValueAndEncoding(v);
+            return value;
+        });
+    }
+}
+
+public enum LedisSetEncoding
+{
+    Hashtable,
 }
