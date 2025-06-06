500d40a7cdad16c53aea3839556819a711542e04
Chien Nguyen Quang <quangchieens1@gmail.com>
2025-06-06 09:36:23 +0700

    chore: add doubly linked list json converter

---
+++ ledis-BE/Converters/DoublyLinkedListConverter.cs
@@ -0,0 +1,46 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ledis_BE.DataStructures;
+
+namespace ledis_BE.Converters;
+
+public class DoublyLinkedListConverter<T> : JsonConverter<DoublyLinkedList<T>>
+{
+    public override DoublyLinkedList<T>? Read(ref Utf8JsonReader reader, Type typeToConvert,
+        JsonSerializerOptions options)
+    {
+        using JsonDocument doc = JsonDocument.ParseValue(ref reader);
+        JsonElement root = doc.RootElement;
+
+        List<SerializableDoublyLinkedListNode<T>>? serializableList =
+            root.Deserialize<List<SerializableDoublyLinkedListNode<T>>>(options);
+        if (serializableList is not null)
+        {
+            DoublyLinkedList<T> linkedList = new DoublyLinkedList<T>();
+            foreach (SerializableDoublyLinkedListNode<T> node in serializableList)
+            {
+                linkedList.AddLast(node.Value);
+            }
+        }
+
+        return null;
+    }
+
+    public override void Write(Utf8JsonWriter writer, DoublyLinkedList<T> value, JsonSerializerOptions options)
+    {
+        List<T> list = value.Values().ToList();
+        List<SerializableDoublyLinkedListNode<T>> serializableList = list
+            .Select(item => new SerializableDoublyLinkedListNode<T>
+            {
+                Value = item,
+            })
+            .ToList();
+
+        JsonSerializer.Serialize(writer, serializableList, options);
+    }
+}
+
+public class SerializableDoublyLinkedListNode<T>
+{
+    public T Value { get; set; }
+}
